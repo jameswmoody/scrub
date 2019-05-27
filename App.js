99e4c4ddffc13beb4c37c6141fde1db52088cd73
James W Moody <james@wmoody.me>
2019-05-27 14:18:00 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './styles';
+import Home from './screens/Home';
+import { 
+  View
+} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Home/>
+      </View>
+    );
+  }
+}
