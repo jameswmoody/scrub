e20564818a8aa196fb6686e1b0210f3d6e85db6c
James W Moody <james@wmoody.me>
2019-05-30 14:33:47 -0700

    feat(edit): Create Edit page

---
+++ screens/Edit.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { imageUpload, deleteImage, updateAbout } from '../redux/actions';
+import styles from '../styles'
+
+import {
+    Text,
+    View,
+    ScrollView,
+    Keyboard,
+    TouchableOpacity,
+    Image,
+    TextInput,
+    KeyboardAvoidingView,
+    TouchableWithoutFeedback
+} from 'react-native';
+
+class Edit extends React.Component {
+    deleteImage() {
+        this.self.props.dispatch(deleteImage(this.self.props.user.images, this.key))
+    }
+
+    addImage() {
+        this.props.dispatch(imageUpload(this.props.user.images))
+    }
+
+    render() {
+        return (
+            <KeyboardAvoidingView
+                behavior='padding'
+                style={{ flex: 1 }}
+            >
+                <View style={[styles.container]}>
+                    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                        <View style={[styles.container, styles.inner]}>
+                            <Text style={styles.paragraph}>Tap an image to delete it</Text>
+                            <View style={styles.imgRow}>
+                                {this.props.user.images.map((uri, index) => {
+                                    return (
+                                        <TouchableOpacity key={index} onPress={this.deleteImage.bind({ self: this, key: index })} >
+                                            <Image style={styles.img} source={{ uri: uri }} />
+                                        </TouchableOpacity>
+                                    );
+                                })}
+                            </View>
+                            <TouchableOpacity style={styles.button} onPress={this.addImage.bind(this)}>
+                                <Text style={styles.buttonText}>Add</Text>
+                            </TouchableOpacity>
+                            <Text style={[styles.paragraph, styles.italic, styles.alignRightText]}>{this.props.user.aboutMe && this.props.user.aboutMe.length}/140</Text>
+                            <TextInput
+                                style={styles.textInput}
+                                multiline={true}
+                                numberOfLines={5}
+                                maxLength={140}
+                                onChangeText={(text) => this.props.dispatch(updateAbout(text))}
+                                value={this.props.user.aboutMe} />
+                            <View style={{ flex: 1 }} />
+                        </View>
+                    </TouchableWithoutFeedback>
+                </View>
+            </KeyboardAvoidingView>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    console.log('### STATE', state)
+    return {
+        user: state.user
+    };
+}
+
+export default connect(mapStateToProps)(Edit);
--- styles.js
+++ styles.js
@@ -5,6 +5,11 @@ const deviceWidth = Dimensions.get('window').width;
 const deviceHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
+    // Alignment
+    alignRightText: {
+        alignSelf: 'stretch',
+        textAlign: 'right'
+    },
     // Element Styling
     main: {
         backgroundColor: '#eaeaea',
@@ -28,6 +33,11 @@ const styles = StyleSheet.create({
         color: '#FFFFFF',
         textTransform: 'uppercase',
         fontWeight: 'bold'
+    },
+    horizontalLine: {
+        borderBottomColor: '#CCCCCC',
+        borderBottomWidth: 1,
+        width: 20
     },
     header1: {
         paddingTop: 30,
@@ -81,6 +91,30 @@ const styles = StyleSheet.create({
         padding: 15,
         backgroundColor: '#fff',
         height: 100
+    },
+    // Edit
+    img: {
+        width: 90,
+        height: 90,
+        borderRadius: 45,
+        margin: 10,
+        backgroundColor: '#fff',
+    },
+    imgRow: {
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        padding: 15,
+    },
+    textInput: {
+        width: deviceWidth,
+        padding: 15,
+        backgroundColor: '#fff',
+        height: 100
+    },
+    inner: {
+        paddingBottom: 100,
+        flex: 1,
+        justifyContent: "flex-end",
     }
 })
 
